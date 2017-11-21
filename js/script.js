fa63725b3a50c086c516bcb195308a08af66fc05
Ashley DeMadet <darthvader@ashleys-mbp.lab.lagcc>
2017-11-20 20:18:50 -0500

    makes fighters

---
+++ js/script.js
@@ -0,0 +1,27 @@
+class Fighter{
+  constructor(name, location, title, weapon, armor, powers, monsterMotto){
+    this.name = name;
+    this.location = location;
+    this.title = title;
+    this.weapon = weapon;
+    this.armor = armor;
+    this.powers = powers;
+    this.health = health;
+    this.monsterMotto = monsterMotto;
+  }
+
+  slogan(){
+    console.log(this.monsterMotto);
+  }
+}
+
+var gsp = new Fighter("GSP", "My House", "welter weight", 'axe','rock armor', "Sleepin' on these hoez", 200, "Is it bedtime yet?");
+var rex = new Fighter("Rex", 'Canada', 'Glorious', 'Sword', 'steel', ['Speed', 'Fire'], 240, 'Prepare to Die');
+var asherDasher = new Fighter("Asher Dasher", "Heaven", "Angel weight", ['Cupid Arrow', 'Angel Dust'], "Diamond" ['Speed', 'Song'], 600, "Prepare to be evaded");
+
+
+function cageFight(fighter1, fighter2, fighter3){
+  if(fighter1.health >)
+
+  }
+}
